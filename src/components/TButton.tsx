d46dec755713fa8a3b19504f0eeb88739dd09d65
NicholasC2 <nicholasc2@proton.me>
2026-06-29 13:04:39 +1200

    fixed disabled

--- src/components/TButton.tsx
+++ src/components/TButton.tsx
@@ -2,15 +2,30 @@
 
 import React from "react";
 
-type Props = React.AnchorHTMLAttributes<HTMLAnchorElement>
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  href?: never;
+  as?: "button";
+};
+
+type LinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  href: string;
+  as: "a";
+};
+
+type Props = ButtonProps | LinkProps;
 
 export default function TButton({ children, className, ...props }: Props) {
+  if (props.as === "a") {
+      return (
+          <a {...props} className={`t-component t-button ${className ?? ""}`}>
+              {children}
+          </a>
+      );
+  }
+
   return (
-    <a
-      {...props}
-      className={`t-component t-button ${className ?? ""}`}
-    >
-      {children}
-    </a>
+      <button {...props} className={`t-component t-button ${className ?? ""}`}>
+          {children}
+      </button>
   );
 }
